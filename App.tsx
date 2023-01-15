26f6873f61f94f4e3364a62c1742a9d4ab41cacb
Orçun Tuna <turboindirblog@gmail.com>
2023-01-15 17:01:00 +0300

    install modules

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
 
 export default function App() {
+
+  const randomWidth = useSharedValue(10);
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}>
+        <Animated.View
+          style={[{ width: 100, height: 80, backgroundColor: 'black', margin: 30 }, style]}
+        />
+        <Button
+          title="toggle"
+          onPress={() => {
+            randomWidth.value = Math.random() * 350;
+          }}
+        />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
